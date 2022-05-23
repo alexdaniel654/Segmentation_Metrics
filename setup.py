7da0b72481d3b3173328216a85630c9b5c5b8529
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2022-05-23 17:06:36 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+# Get requirements from text file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Use README.md as the long description
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="segmentationmetrics",
+    version="0.0.1",
+    description="Binary segmentation accuracy metrics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexdaniel654/Segmentation_Metrics",
+    license="Apache-2.0",
+
+    python_requires='>=3.8, <4',
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
