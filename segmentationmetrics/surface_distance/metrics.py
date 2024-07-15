31a7e8df5276e84dffe8cbcdeaaedb41b7c3403b
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2024-07-15 12:04:05 +0100

    Same with np.NaN

--- segmentationmetrics/surface_distance/metrics.py
+++ segmentationmetrics/surface_distance/metrics.py
@@ -438,6 +438,6 @@ def compute_dice_coefficient(mask_gt, mask_pred):
   """
   volume_sum = mask_gt.sum() + mask_pred.sum()
   if volume_sum == 0:
-    return np.NaN
+    return np.nan
   volume_intersect = (mask_gt & mask_pred).sum()
   return 2*volume_intersect / volume_sum
